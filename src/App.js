cb120748ddc754eb1f495921d446c421e15acc36
hiran <hiranthaathapaththu@gmail.com>
2019-09-22 20:27:28 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -3,97 +3,39 @@ import React, { Component } from 'react';
 import './App.css';
 import Display from './components/Display';
 import axios from 'axios';
+import { SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS } from 'constants';
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.requestAuth = this.requestAuth.bind(this);
+    this.submitProfile = this.submitProfile.bind(this);
+    this.goHome = this.goHome.bind(this);
 
     this.state = {
       urlparams : null,
       redirect_uri : null,
-      accessToken : null
+      home_url : null,
+      accessToken : null,
+      display : false,
+      profileInfo : null,
+      email : null,
+      state : null
     }
   }
 
   UNSAFE_componentWillMount(){
     var pathname = window.location.pathname;
-    console.log(pathname);
+    var homeurl=window.location.protocol+'//' + window.location.hostname  +":"+window.location.port;
+    this.setState({home_url:homeurl});
     var params = this.getAllUrlParams();
-    console.log(params);
     this.setState({urlparams : params});
     if(pathname==="/auth/linkedin/callback"){
-      console.log("authcode recieved");
       var redirecturi=window.location.protocol+'//' + window.location.hostname  +":"+window.location.port+"/auth/linkedin/callback";
       this.requestToken(params.code,redirecturi);
     }
   }
-
-  getAllUrlParams(url) {
-	
-		// get query string from url (optional) or window
-		var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
-	  
-		// we'll store the parameters here
-		var obj = {};
-	  
-		// if query string exists
-		if (queryString) {
-	  
-		  // stuff after # is not part of query string, so get rid of it
-		  queryString = queryString.split('#')[0];
-	  
-		  // split our query string into its component parts
-		  var arr = queryString.split('&');
-	  
-		  for (var i = 0; i < arr.length; i++) {
-			// separate the keys and the values
-			var a = arr[i].split('=');
-	  
-			// set parameter name and value (use 'true' if empty)
-			var paramName = a[0];
-			var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
-	  
-			// (optional) keep case consistent
-			// paramName = paramName.toLowerCase();
-			// if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase();
-	  
-			// if the paramName ends with square brackets, e.g. colors[] or colors[2]
-			if (paramName.match(/\[(\d+)?\]$/)) {
-	  
-			  // create key if it doesn't exist
-			  var key = paramName.replace(/\[(\d+)?\]/, '');
-			  if (!obj[key]) obj[key] = [];
-	  
-			  // if it's an indexed array e.g. colors[2]
-			  if (paramName.match(/\[\d+\]$/)) {
-				// get the index value and add the entry at the appropriate position
-				var index = /\[(\d+)\]/.exec(paramName)[1];
-				obj[key][index] = paramValue;
-			  } else {
-				// otherwise add the value to the end of the array
-				obj[key].push(paramValue);
-			  }
-			} else {
-			  // we're dealing with a string
-			  if (!obj[paramName]) {
-				// if it doesn't exist, create property
-				obj[paramName] = paramValue;
-			  } else if (obj[paramName] && typeof obj[paramName] === 'string'){
-				// if property does exist and it's a string, convert it to an array
-				obj[paramName] = [obj[paramName]];
-				obj[paramName].push(paramValue);
-			  } else {
-				// otherwise add the property
-				obj[paramName].push(paramValue);
-			  }
-			}
-		  }
-		}
-	  
-		return obj;
-	}
 
   requestAuth(){
     var redirecturi=window.location.protocol+'//' + window.location.hostname  +":"+window.location.port+"/auth/linkedin/callback";
@@ -103,11 +45,10 @@ class App extends Component {
   }
 
   requestToken(authcode,redirecturi){
-    console.log("req token clicked");
-    console.log(authcode);
+    this.setState({submitStatus:"loading"});
 
     var url = "http://localhost:8080/https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code&code="+authcode+"&redirect_uri="+redirecturi+"&client_id=81uv5q50q6h4va&client_secret=peuX5PsUcpv3nFYJ";
-    // axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+
     var headers = {
       'Access-Control-Allow-Origin': '*',
       'Content-Type': 'application/json',
@@ -115,21 +56,131 @@ class App extends Component {
 
     axios.post(url,headers).then(function (response) {
             console.log(response);
-            this.setState({htmlres:response.data});
             return response.data;
         }.bind(this)).then(function (data) {
             console.log(data);
-            this.setState({accessToken : data.access_token});
+            this.requestInfo(data.access_token);
+            this.requestEmail(data.access_token);
 
         }.bind(this));
+  }
+
+  requestInfo(token){
+
+    var url = "http://localhost:8080/https://api.linkedin.com/v2/me?projection=(id,firstName,lastName,profilePicture(displayImage~:playableStreams))";
+
+    axios.get(url,{ headers: { Authorization: 'Bearer '+token } }).then(function (response) {
+
+            return response.data;
+        }.bind(this)).then(function (data) {
+            this.setState({display : true,profileInfo:data});
+            this.setState({submitStatus : "loaded"});
+        }.bind(this));
+  }
+
+  requestEmail(token){
+
+    var url = "http://localhost:8080/https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))";
+
+    axios.get(url,{ headers: { Authorization: 'Bearer '+token } }).then(function (response) {
+            return response.data;
+        }.bind(this)).then(function (data) {
+            this.setState({email:data.elements[0]["handle~"].emailAddress});
+        }.bind(this));
+  }
+
+  getAllUrlParams(url) {
+	
+		var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+		var obj = {};
+		if (queryString) {
+		  queryString = queryString.split('#')[0];
+		  var arr = queryString.split('&');
+	  
+		  for (var i = 0; i < arr.length; i++) {
+			var a = arr[i].split('=');
+			var paramName = a[0];
+			var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
+
+			if (paramName.match(/\[(\d+)?\]$/)) {
+			  var key = paramName.replace(/\[(\d+)?\]/, '');
+        if (!obj[key]) obj[key] = [];
+			  if (paramName.match(/\[\d+\]$/)) {
+				  var index = /\[(\d+)\]/.exec(paramName)[1];
+				  obj[key][index] = paramValue;
+        } 
+        else {
+				  obj[key].push(paramValue);
+			  }
+			} else {
+			  if (!obj[paramName]) {
+				  obj[paramName] = paramValue;
+			  } else if (obj[paramName] && typeof obj[paramName] === 'string'){
+          obj[paramName] = [obj[paramName]];
+          obj[paramName].push(paramValue);
+			  } else {
+				  obj[paramName].push(paramValue);
+			  }
+			}
+		  }
+		}
+	  
+		return obj;
+	}
+
+  submitProfile(){
+    this.setState({submitStatus:"submitted"});
+  }
+
+  goHome(){
+    window.open(this.state.home_url,"_self");
   }
 
   render() {
 
     var right_content;
+    var left_content;
 
-    if(this.state.accessToken!==null){
-      right_content = <Display token={this.state.accessToken}/>
+    right_content = <div className="App-header "> 
+      <img  className="home-img" src="./findajob.png" alt="logo"/>
+    </div>
+
+    if(this.state.display){
+      right_content = <Display info={this.state.profileInfo} email={this.state.email}/>
+      left_content = <div className="App-header">
+          <h1>Hello {this.state.profileInfo.firstName.localized.en_US},</h1>
+          <p className="padding-10-left">One more step to go. Confirm your submission by clicking on submit profile.</p>
+          <a href="#" class="btn btn-primary" onClick={this.submitProfile}>Submit Profile</a>
+      </div>
+    }else{
+      left_content = <div className="App-header">
+        <h1>Submit your Linkedin account. We will get you a job.</h1>
+        <p className="App-link">
+        Simple as that!
+        </p>
+        
+        <br/>
+        <img src="./Sign-In-Large---Active.png" className="App-logo" alt="logo" onClick={this.requestAuth}/>
+      </div>
+    }
+
+    if(this.state.submitStatus==="submitted"){
+      left_content = <div>
+        <h1>Profile submitted. We will contact you soon.</h1>
+        <br/>
+        <a href="#" class="btn btn-secondary" onClick={this.goHome}>Go Home</a>
+      </div>
+    }
+    else if(this.state.submitStatus==="loading"){
+      left_content = <div>
+        <div class="spinner-border text-danger" role="status">
+          <span class="sr-only">Loading...</span>
+        </div>
+        <br/>
+        <br/>
+        <a href="#" class="btn btn-secondary" onClick={this.goHome}>Cancel</a>
+      </div>
+      right_content = ""
     }
 
     return (
@@ -138,20 +189,7 @@ class App extends Component {
           <div className="row">
               <div className="intro col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6">
                 <header className="App-header">
-                    <h1>Upload your CV. We will get you a job.</h1>
-                    <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    >
-                    Learn React
-                    </a>
-                    
-                    <img src="./Sign-In-Large---Active.png" className="App-logo" alt="logo" onClick={this.requestAuth}/>
+                    {left_content}
                 </header>
               </div>
               <div className="content col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6">
--- src/components/Display.js
+++ src/components/Display.js
@@ -5,35 +5,23 @@ class Display extends Component {
 
     constructor(props) {
       super(props);
-      console.log(props);
       this.state = {
 
       }
     }
-
-    componentDidMount(){
-        this.requestInfo(this.props.token);
-    }
-
-    requestInfo(token){
-        console.log("req info clicked");
-        console.log(token);
-    
-        var url = "http://localhost:8080/https://api.linkedin.com/v2/me?projection=(id,firstName,lastName,profilePicture(displayImage~:playableStreams))";
-
-        axios.get(url,{ headers: { Authorization: 'Bearer '+token } }).then(function (response) {
-                console.log(response);
-                return response.data;
-            }.bind(this)).then(function (data) {
-                console.log(data);
-            }.bind(this));
-    }
     
     render() {
-
         return (
             <div className="display-content">
-                <p className="content-fonts-normal">Lets get started.</p>
+                <br/>
+                <br/>
+                <div class="card profile-pic-card ">
+                    <img class="card-img-top" src={this.props.info.profilePicture["displayImage~"].elements[2].identifiers[0].identifier} alt="Card image"/>
+                    <div class="card-body">
+                    <h4 class="card-title">{this.props.info.firstName.localized.en_US} {this.props.info.lastName.localized.en_US}</h4>
+                    <p class="card-text">Email : {this.props.email}</p>
+                    </div>
+                </div>
 
             </div>
         );
